b20a6303e09718fec718fce495c7769be660dbba
youngjun.jin <yzzzzun@gmail.com>
2021-07-23 01:14:37 +0900

    feat: item validator 구현

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -28,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ValidationItemControllerV2 {
 
 	private final ItemRepository itemRepository;
+	private final ItemValidator itemValidator;
 
 	@GetMapping
 	public String items(Model model) {
@@ -172,7 +173,7 @@ public class ValidationItemControllerV2 {
 		return "redirect:/validation/v2/items/{itemId}";
 	}
 
-	@PostMapping("/add")
+	// @PostMapping("/add")
 	public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
 		RedirectAttributes redirectAttributes,
 		Model model) {
@@ -201,6 +202,24 @@ public class ValidationItemControllerV2 {
 			}
 		}
 
+		if (bindingResult.hasErrors()) {
+			log.info("error={}", bindingResult);
+			return "validation/v2/addForm";
+		}
+
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+	@PostMapping("/add")
+	public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+		RedirectAttributes redirectAttributes,
+		Model model) {
+
+		itemValidator.validate(item, bindingResult);
+		
 		if (bindingResult.hasErrors()) {
 			log.info("error={}", bindingResult);
 			return "validation/v2/addForm";
