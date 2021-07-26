5b1ef4144538db4739fa9711c1cbd8a941650a1b
youngjun.jin <yzzzzun@gmail.com>
2021-07-26 15:02:40 +0900

    feat: bean validation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,21 +1,35 @@
 package hello.itemservice.domain.item;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
 import lombok.Data;
 
 @Data
 public class Item {
 
-    private Long id;
-    private String itemName;
-    private Integer price;
-    private Integer quantity;
+	private Long id;
 
-    public Item() {
-    }
+	@NotBlank
+	private String itemName;
 
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
+	@NotNull
+	@Range(min = 1000, max = 1000000)
+	private Integer price;
+
+	@NotNull
+	@Max(9999)
+	private Integer quantity;
+
+	public Item() {
+	}
+
+	public Item(String itemName, Integer price, Integer quantity) {
+		this.itemName = itemName;
+		this.price = price;
+		this.quantity = quantity;
+	}
 }
