5424cb0fc76e410003812189444bf5bd52143752
youngjun.jin <yzzzzun@gmail.com>
2021-07-26 20:23:39 +0900

    feat: 인증 필터 추가

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import lombok.extern.java.Log;
 
 @Configuration
@@ -18,6 +19,16 @@ public class WebConfig {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new LogFilter());
 		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.addUrlPatterns("/*");
+
+		return filterRegistrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean loginCheckFilter(){
+		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new LoginCheckFilter());
+		filterRegistrationBean.setOrder(2);
 		filterRegistrationBean.addUrlPatterns("/*");
 
 		return filterRegistrationBean;
