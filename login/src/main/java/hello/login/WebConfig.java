7eb1e91f3bb4e39cb3854561c8afaafaf633b8df
youngjun.jin <yzzzzun@gmail.com>
2021-07-26 22:32:48 +0900

    feat: spring interceptor로 logInterceptor 구현

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -6,13 +6,23 @@ import javax.servlet.FilterRegistration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
-import lombok.extern.java.Log;
+import hello.login.web.interceptor.LogInterceptor;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/css/**", "/*.ico", "/error");
+	}
 
 	@Bean
 	public FilterRegistrationBean logFilter(){
